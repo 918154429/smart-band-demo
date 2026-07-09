3fbfc41d7c7c11cb34378bb864cde47d12f5273e
dhy <1795843039@qq.com>
2026-07-09 10:06:57 +0800

    Revert "Rotate smart band display"
    This reverts commit 63da5a68d4beb141f6d9180b537d9f6ea8e7cc8a.

--- openvela_app/smart_band/app_lvgl.c
+++ openvela_app/smart_band/app_lvgl.c
@@ -14,7 +14,6 @@
 #define DESIGN_H 626
 #define CARD_COUNT 4
 #define SMART_BAND_DEFAULT_TZ "CST-8"
-#define SMART_BAND_DISPLAY_ROTATION LV_DISPLAY_ROTATION_90
 
 typedef struct
 {
@@ -257,17 +256,6 @@ static void format_duration(char *buffer, size_t size, int seconds)
     }
 
   snprintf(buffer, size, "%02d:%02d", seconds / 60, seconds % 60);
-}
-
-static void configure_display_rotation(void)
-{
-  lv_display_t *display = lv_display_get_default();
-
-  if (display != NULL)
-    {
-      lv_display_set_matrix_rotation(display, true);
-      lv_display_set_rotation(display, SMART_BAND_DISPLAY_ROTATION);
-    }
 }
 
 static void set_temperature_label(lv_obj_t *label)
@@ -1488,10 +1476,7 @@ static void enable_touch_navigation_tree(lv_obj_t *obj)
 
 int smart_band_lvgl_create(lv_obj_t *parent)
 {
-  lv_obj_t *root;
-
-  configure_display_rotation();
-  root = parent != NULL ? parent : lv_scr_act();
+  lv_obj_t *root = parent != NULL ? parent : lv_scr_act();
 
   if (root == NULL)
     {
